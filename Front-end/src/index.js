041ff7598305e117dd478a64b969f4ba13f94f8c
Lior <lior4007@gmail.com>
2023-08-08 20:52:48 +0300

    changes in files

--- Front-end/src/index.js
+++ Front-end/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'tachyons';
 import { BusinessProvider } from "./context/BusinessContext"
 
 ReactDOM.render(
