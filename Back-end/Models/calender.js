39119ad92f709d6441d97d347e19127904292ad2
Lior <lior4007@gmail.com>
2023-08-10 20:02:00 +0300

    changes name folders

--- Back-end/Models/calender.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const CalenderSchema = mongoose.Schema({
-    businessID: String,
-    dates: Array,
-    availableHours: Array,
-})
-
-module.exports = mongoose.model("Calender", CalenderSchema);
